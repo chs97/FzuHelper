970bfa94a45482c856526ab4dbd1904d3e6d0d08
Chs97 <623528324@qq.com>
2017-11-27 18:42:20 +0800

    fixed login

--- srv/jwch/handler/jwch.go
+++ srv/jwch/handler/jwch.go
@@ -1,6 +1,9 @@
 package handler
 
 import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
 	"errors"
 	"io/ioutil"
 	"net/http"
@@ -15,6 +18,30 @@ import (
 
 type Jwch struct{}
 
+type ocr struct {
+	Base64    string `json:"base64"`
+	Trim      string `json:"trim"`
+	whitelist string `json:"whitelist"`
+}
+
+type ocrResult struct {
+	Result  string `json:"result"`
+	Version string `json::"version"`
+}
+
+func captcha(base64 string) string {
+	data := ocr{Base64: base64, Trim: "\n", whitelist: "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"}
+	buf := new(bytes.Buffer)
+	json.NewEncoder(buf).Encode(data)
+	res, err := http.Post("http://localhost:8088/base64", "application/json; charset=utf-8", buf)
+	if err != nil {
+		return ""
+	}
+	resData := new(ocrResult)
+	json.NewDecoder(res.Body).Decode(resData)
+	return resData.Result
+}
+
 func getClient(stdno, passwd string) (http.Client, string, error) {
 	var client http.Client
 	jar, err := cookiejar.New(nil)
@@ -22,7 +49,21 @@ func getClient(stdno, passwd string) (http.Client, string, error) {
 		return client, "", err
 	}
 	client.Jar = jar
-	form := url.Values{"muser": {stdno}, "passwd": {passwd}}
+	getCapReq, err := http.NewRequest("GET", "http://59.77.226.32/captcha.asp?time=0.9019912959584149", nil)
+	if err != nil {
+		return client, "", err
+	}
+	getCapRes, err := client.Do(getCapReq)
+	if err != nil {
+		return client, "", err
+	}
+	capBody, err := ioutil.ReadAll(getCapRes.Body)
+	if err != nil {
+		return client, "", err
+	}
+	base := base64.StdEncoding.EncodeToString(capBody)
+	capResult := captcha(base)
+	form := url.Values{"muser": {stdno}, "passwd": {passwd}, "Verifycode": {capResult}}
 	req, err := http.NewRequest("POST", "http://59.77.226.32/logincheck.asp", strings.NewReader(form.Encode()))
 	if err != nil {
 		return client, "", err
@@ -40,9 +81,11 @@ func getClient(stdno, passwd string) (http.Client, string, error) {
 		return client, "", err
 	}
 	result := string(body)
-	alert := strings.Index(result, "alert")
-	if alert != -1 {
+	if len(result) == 193 {
 		return client, "", errors.New("Username or Password error")
+	}
+	if len(result) == 167 {
+		return client, "", errors.New("Captcha Error")
 	}
 	start := strings.Index(result, "top.aspx?id=")
 	end := strings.Index(result, "\" noresize=\"noresize\"")
@@ -63,13 +106,24 @@ func (j *Jwch) Login(ctx context.Context, req *jwch.LoginRequest, rsp *jwch.Logi
 	return nil
 }
 
+// Getinfo asdasdsa
 func (j *Jwch) Getinfo(ctx context.Context, req *jwch.GetInfoRequest, rsp *jwch.GetInfoResponse) error {
 	stdno := req.Stdno
 	passwd := req.Password
-	client, id, err := getClient(stdno, passwd)
+	var client http.Client
+	var id string
+	var err error
+	for {
+		client, id, err = getClient(stdno, passwd)
+		if err != nil && err.Error() == "Captcha Error" {
+			continue
+		}
+		break
+	}
 	if err != nil {
 		return err
 	}
+
 	url := "http://59.77.226.35/jcxx/xsxx/StudentInformation.aspx?id=" + id
 	jreq, err := http.NewRequest("GET", url, nil)
 	if err != nil {
